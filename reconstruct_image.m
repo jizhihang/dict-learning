e4720f7f7a08c01f0535722021a698a95fde39ef
Nishanth Koushik <nnk@cse.iitb.ac.in>
2015-09-03 13:25:49 +0530

    Change reconstruct_image to grayscale only

--- reconstruct_image.m
+++ reconstruct_image.m
@@ -6,60 +6,54 @@ half = (patch_size - 1) / 2;
 I = double(imread(in_name));
 x = size(I, 1);
 y = size(I, 2);
-c = size(I, 3);
 max_num_patches = ((x - patch_size - 1) * (y - patch_size - 1));
 
-new_I = zeros(x, y, c);
+new_I = zeros(x, y);
 
-parfor col = 1:c
-  counter = 0;
-  X = zeros(patch_size^2, max_num_patches);
-  means = zeros(1, max_num_patches);
-  vars = zeros(1, max_num_patches);
-  index = zeros(x, y);
+counter = 0;
+X = zeros(patch_size^2, max_num_patches);
+means = zeros(1, max_num_patches);
+vars = zeros(1, max_num_patches);
+index = zeros(x, y);
 
-  for i = (1 + half):(x - half)
-    for j = (1 + half):(y - half)
-      patch = I((i - half):(i + half), (j - half):(j + half), col);
-      patch = reshape(patch, patch_size^2, 1);    
-      sigma = var(patch);
+for i = (1 + half):(x - half)
+  for j = (1 + half):(y - half)
+    patch = I((i - half):(i + half), (j - half):(j + half));
+    patch = reshape(patch, patch_size^2, 1);    
+    sigma = var(patch);
 
-      if (sigma > var_thresh)
-        mu = mean(patch);
-        patch = (patch - mu) / sigma;
-        counter = counter + 1;
-        X(:, counter) = patch;
-        means(counter) = mu;
-        vars(counter) = sigma;
-        index(i, j) = counter;
-      end
+    if (sigma > var_thresh)
+      mu = mean(patch);
+      patch = (patch - mu) / sigma;
+      counter = counter + 1;
+      X(:, counter) = patch;
+      means(counter) = mu;
+      vars(counter) = sigma;
+      index(i, j) = counter;
     end
   end
-
-  X = X(:, 1:counter);
-  [W, D] = dict_learning(X, 50);
-  new_X = normc(D * W);
-
-  col_I = zeros(x, y);
-  count = zeros(x, y);
-
-  for i = (1 + half):(x - half)
-    for j = (1 + half):(y - half)
-      if(index(i, j) > 0)
-        patch = (new_X(:, index(i, j)) * vars(index(i, j))) + ...
-          means(index(i, j));
-        col_I((i - half):(i + half), (j - half):(j + half)) = ...
-          col_I((i - half):(i + half), (j - half):(j + half)) + ...
-          reshape(patch, patch_size, patch_size);
-        count((i - half):(i + half), (j - half):(j + half)) = ...
-          count((i - half):(i + half), (j - half):(j + half)) + ...
-          ones(patch_size, patch_size);
-      end
-    end
-  end
-
-  new_I(:, :, col) = col_I ./ count;
 end
 
-imwrite(uint8(new_I), out_name);
+X = X(:, 1:counter);
+[W, D] = dict_learning(X, 50);
+new_X = normc(D * W);
+
+count = zeros(x, y);
+
+for i = (1 + half):(x - half)
+  for j = (1 + half):(y - half)
+    if(index(i, j) > 0)
+      patch = (new_X(:, index(i, j)) * vars(index(i, j))) + ...
+        means(index(i, j));
+      new_I((i - half):(i + half), (j - half):(j + half)) = ...
+        new_I((i - half):(i + half), (j - half):(j + half)) + ...
+        reshape(patch, patch_size, patch_size);
+      count((i - half):(i + half), (j - half):(j + half)) = ...
+        count((i - half):(i + half), (j - half):(j + half)) + ...
+        ones(patch_size, patch_size);
+    end
+  end
+end
+
+imwrite(uint8(new_I ./ count), out_name);
 end
